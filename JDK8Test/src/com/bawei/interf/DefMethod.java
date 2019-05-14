e6b091bfa7a0bdf4998152f64755fba64a2d7da6
Shenpanpan123 <m18040030328@qq.com>
2019-05-14 13:26:29 +0800

    我想提交项目到GitHub的远程仓库中

---
+++ JDK8Test/src/com/bawei/interf/DefMethod.java
@@ -0,0 +1,17 @@
+package com.bawei.interf;
+
+
+@FunctionalInterface
+public interface DefMethod<T,F> {
+	
+	public F funtion1(T t);
+	
+	public default void funtion2(){
+		System.out.println("default funtion2");
+	}
+	
+	public static void funtion3(){
+		System.out.println("static funtion3");
+	}
+	
+}
