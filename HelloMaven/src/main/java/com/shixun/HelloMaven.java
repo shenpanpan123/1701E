17d52da5d646c8d6532640854677d4c4b9c26de6
Shenpanpan123 <m18040030328@qq.com>
2019-05-14 15:33:55 +0800

    第二次提交

--- HelloMaven/src/main/java/com/shixun/HelloMaven.java
+++ HelloMaven/src/main/java/com/shixun/HelloMaven.java
@@ -3,5 +3,8 @@ package com.shixun;
 public class HelloMaven {
 	public void sayhello(){
 		System.out.println("hello 1612a");
+		
+		System.out.println("第二次提交");
+		System.out.println("Hello git");
 	}
 }
